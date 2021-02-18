9b8088a9aa2903b15455e9186a3fe37ae0e1a2f6
YAO BESSE <yaoolivierbesse@gmail.com>
2021-02-18 11:36:16 +0100

    Premier commit

--- README.md
+++ README.md
@@ -1,1 +1,5 @@
-# montecarlo
+# projet_si_MBR
+
+# Ajout de besse 18.02.2021 :
+- Modification de projet/CMakeLists.txt
+- Ajout du dossier arbre_binaire
---
+++ joueurs/joueur_montecarlo_.hh
@@ -0,0 +1,17 @@
+#pragma once
+
+#include "../../projet/arbre.hh"
+#include "joueur.hh"
+#include "joueur_random.hh"
+
+
+class Joueur_MonteCarlo_ : public Joueur
+{
+public:
+  Joueur_MonteCarlo_(std::string nom, bool joueur);
+  //char nom_abbrege() const override;
+
+  void recherche_coup(Jeu, Brix & coup) override;
+
+  //arbreNaire fileToArbre (std::vector<unsigned int> &v)
+};
